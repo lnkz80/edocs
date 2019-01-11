a91f10f719c30512046a3af20f98ba4f9dc99967
lnkz80 <lnkz80@gmail.com>
2019-01-11 16:14:55 +0200

    editing Lib model for MVO view - table style

--- app/views/lib/view.php
+++ app/views/lib/view.php
@@ -1,14 +1,22 @@
-<table>
-    <?php if($id == 'mvo'): ?>
-<?php foreach ($mvoarr as $item=>$value):?>
-
+<table class="table table-hover table-sm">
+    <thead class="thead-light">
     <tr>
-        <td><?php echo $value['id'] ?></td>
-        <td><?php echo $value['name'] ?></td>
-        <td><?php echo $value['department'] ?></td>
-        <td><?php echo $value['location'] ?></td>
+        <th>#</th>
+        <th>Наименование</th>
+        <th>Отдел</th>
+        <th>Расположение</th>
     </tr>
+    </thead>
+    <?php if($id == 'mvo'): ?>
+        <?php foreach ($mvoarr as $item=>$value):?>
 
-<?php endforeach; ?>
+            <tr>
+                <td><?php echo $value['id'] ?></td>
+                <td><?php echo $value['name'] ?></td>
+                <td><?php echo $value['department'] ?></td>
+                <td><?php echo $value['location'] ?></td>
+            </tr>
+
+        <?php endforeach; ?>
     <?php endif; ?>
 </table>
