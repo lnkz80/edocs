b9708dd084d5bc60c2e578badda5c56f2c9a098f
lnkz80 <lnkz80@gmail.com>
2019-02-15 16:12:40 +0200

    editing Lib model for MVO view - table style

--- app/views/cartridges/actions.php
+++ app/views/cartridges/actions.php
@@ -1,20 +1,67 @@
 <h3>Операции с картриджами</h3>
 <hr>
 <form action="">
-    От:<select name="" id="">
-        <option value="">Иванов</option>
-        <option value="">Петров</option>
-        <option value="">Сидоров</option>
-    </select>
-    Кому:<select name="" id="">
-        <option value="">Иванов</option>
-        <option value="">Петров</option>
-        <option value="">Сидоров</option>
-    </select>
-    <select name="" id="">№
-        <option value="">1234</option>
-        <option value="">2345</option>
-        <option value="">3456</option>
-    </select>
-    <label for="">Комментарий:</label><input type="text">
+    <fieldset class="form-group">
+        <legend>Картридж №1</legend>
+
+    <div class="form-row">
+        <div class="col-2">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect01"><i class="fas fa-male"></i>&nbsp;<i class="fas fa-arrow-right"></i></label>
+                </div>
+                <select class="custom-select" id="inputGroupSelect01">
+                    <option selected>МВО</option>
+                    <option value="1">Иванов И.И.</option>
+                    <option value="2">Петров П.П.</option>
+                    <option value="3">Сидоров А.А.</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect02"><i class="fas fa-arrow-right"></i>&nbsp;<i class="fas fa-male"></i></label>
+                </div>
+                <select class="custom-select" id="inputGroupSelect02">
+                    <option selected>МВО</option>
+                    <option value="1">Иванов И.И.</option>
+                    <option value="2">Петров П.П.</option>
+                    <option value="3">Сидоров А.А.</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect03">#</label>
+                </div>
+                <select class="custom-select" id="inputGroupSelect03">
+                    <option selected>Номер</option>
+                    <option value="1">1234</option>
+                    <option value="2">2345</option>
+                    <option value="3">3456</option>
+                </select>
+            </div>
+        </div>
+
+
+        <div class="col-4">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1"><i class="far fa-edit"></i></span>
+                </div>
+                <input type="text" class="form-control" placeholder="Комментарий" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+        </div>
+        <div class="col-1" style="text-align: right;">
+            <a class="btn btn-danger" href="#">
+                <i class="fas fa-window-close"></i></a>
+        </div>
+        <div class="col-1">
+            <a class="btn btn-success" href="#">
+                <i class="fas fa-plus-square"></i></a>
+        </div>
+    </div>
+    </fieldset>
 </form>
