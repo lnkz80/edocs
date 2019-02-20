de51ab258c9005d9b9b03a415c82e13c32a82759
lnkz80 <lnkz80@gmail.com>
2019-02-20 16:51:44 +0200

    editing Lib model for MVO view - table style

--- app/js/common.js
+++ app/js/common.js
@@ -1,86 +1,9 @@
+$(document).ready(function() {
+
+    // Executes when the HTML document is loaded and the DOM is ready
+    // alert("Document is ready");
+});
+
 $(function(){
-
-	$(".lside li").click(function(){
-
-		var obj = $(this);
-		var actid = obj.attr("item-id");
-		var ul = obj.parent();
-		var ulid = ul.attr("id");
-		var objects = "#" +ulid + " li";
-		var colors = ["coral", "green", "purple", "brown", "navy"];
-
-		$(objects).find($('span')).hide(200);
-		
-		if((ulid!="mnu1") && (ulid!="mnu3")){
-			$(ul).find('.headerli').remove();
-		}
-
-		if(ulid!="mnu3"){
-			nextulid = $(ul).next().attr("id");
-			objectsnext = "#" + nextulid + " li";
-			$(objectsnext).each(function(i, elem){
-				if (obj.attr("item-id") == $(elem).attr("item-par")){
-					$(elem).show();					
-				}
-				else{
-					$(elem).hide();
-				}				
-			});
-
-			$(ul).next().css("visibility", "visible");		
-		}
-			
-		$(objects).each(function(i) {
-            if ($(this).attr("item-id") == actid) {
-                $(this).addClass('lside_mnu_active')
-                    .find("img").attr({ "style": "background-color: " + colors[i] })
-            } else {
-                $(this).removeClass('lside_mnu_active')
-                    .find("img").attr({ "style": "background-color: #999" })
-					}
-		});
-
-		if(ulid == "mnu1"){			
-			$(ul).next().find($('span')).show(200);
-			// $(ul).next().find($('span')).show(200);
-			
-			$("#mnu2, #mnu3").find("li").removeClass('lside_mnu_active');
-			$("#mnu2, #mnu3").find("img").attr({ "style": "background-color: #999"});
-			
-			// if($("#mnu2 li").hasClass("headerli") != true){
-			// 	$("#mnu2 li:eq(0)").before('<li class="headerli"><span>Активы</span></li>');				
-			// }
-
-			// obj.addClass('lside_mnu_active');			
-			$("#mnu3").css("visibility", "hidden");
-		}
-
-		// if (ulid == "mnu2"){
-		// 	$("#mnu3").find("li").removeClass('lside_mnu_active');
-		// }
-		
-		// console.log(ulid);
-	});
-
-// 	$("#mnu1 li").click(function() {
-//         var spans = $('span');
-//         var clobj = $(this);
-//         var actId = clobj.attr("item-id");
-//         var colors = ["coral", "green", "purple", "brown"];
-
-//         $("#mnu1 li").find(spans).hide(200);
-
-//         $("#mnu2").css("visibility", "visible");
-//         $("#mnu1 li").each(function(i) {
-//             if ($(this).attr("item-id") == actId) {
-//                 $(this).addClass('lside_mnu_active')
-//                     .find("img").attr({ "style": "background-color: " + colors[i] })
-//             } else {
-//                 $(this).removeClass('lside_mnu_active')
-//                     .find("img").attr({ "style": "background-color: #999" })
-// 					}
-// 		});
-// // console.log(clobj.attr("item-id"));
-// 	});
 
 });
--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,14 +1,14 @@
         </div>
     </div>
 </div>
-<div class="row page-footer">
-    <div class="col-md-12 foot">
-        Alex Kocherga ©2018
+    <div class="row page-footer">
+        <div class="col-md-12 foot">
+            Alex Kocherga ©2018
+        </div>
     </div>
 </div>
-</div>
-
-
-<script src="js/libs.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="/js/libs.js"></script>
+        <script src="/js/common.js"></script>
 </body>
 </html>
